e25a8f18fb731838d4fa15d94159c5d77003db84
Nosima <breno.nosima@gmail.com>
2026-03-23 13:35:02 -0300

    model criada

--- src/main/java/com/bn/projetoscs/models/ProjetoModel.java
+++ src/main/java/com/bn/projetoscs/models/ProjetoModel.java
@@ -1,4 +1,54 @@
 package com.bn.projetoscs.models;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_projeto")
 public class ProjetoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "nome")
+    private String nome;
+    @Column(name = "dataInicio")
+    private LocalDate dataInicio;
+    private LocalDate dataFim;
+
+    public ProjetoModel() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(LocalDate dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public LocalDate getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(LocalDate dataFim) {
+        this.dataFim = dataFim;
+    }
 }
